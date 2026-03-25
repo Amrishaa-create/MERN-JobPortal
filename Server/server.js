da0f3cd09d2d013d9dfeb7e2cb769ffeef867a8f
Amrishaa <vijayarathivijayarathi@gmail.com>
2026-03-25 17:20:44 +0530

    force fix cors headers

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import cors from 'cors'
+// import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import dns from 'node:dns/promises'
 import connectDB from './config/db.js'
@@ -31,29 +31,23 @@ const allowedOrigins = [
   "https://mern-job-portal-yunu.vercel.app",
   "https://mern-job-portal-elsq.vercel.app"
 ]
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true
- }))
 
-// 
+app.options("*", cors({
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("CORS not allowed"));
+    }
+  },
+  credentials: true
+}))
 
 // app.use(cors({
-//   origin: function(origin, callback) {
-//     if (!origin) return callback(null, true)
-//     if (allowedOrigins.indexOf(origin) === -1) {
-//       return callback(new Error('Not allowed by CORS'), false)
-//     }
-//     return callback(null, true)
-//   },
-//   credentials: true,
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-//   allowedHeaders: ["Content-Type", "Authorization"]
-// }))
-// app.options("*", cors({
 //   origin: allowedOrigins,
 //   credentials: true
-// }));
+//  }))
  
 app.use('/api/auth',authRoutes)
 app.use('/api/admin/jobs',adminJobRoutes)
